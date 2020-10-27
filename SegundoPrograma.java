ec8e315e33f19f55258e06d23be9d778c25013f9
Sara de Paula Teixeira <73507754+SaraTx-code@users.noreply.github.com>
2020-10-27 14:11:06 -0300

    Update SegundoPrograma.java

--- SegundoPrograma.java
+++ SegundoPrograma.java
@@ -2,9 +2,8 @@
 package segundoprograma;
 
 public class SegundoPrograma {
-
+    
     public static void main(String[] args) {
         System.out.print("Olá, mundo. Hello!");
-    }
-    
+    } 
 }
