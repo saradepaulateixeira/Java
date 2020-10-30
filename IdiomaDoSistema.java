9bd723f11797761979a631714aa737ba7b907d7b
Sara de Paula Teixeira <73507754+SaraTx-code@users.noreply.github.com>
2020-10-30 14:20:36 -0300

    Add files via upload

---
+++ IdiomaDoSistema.java
@@ -0,0 +1,14 @@
+
+package idiomadosistema;
+
+import java.util.Locale;
+
+public class IdiomaDoSistema {
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale.getDisplayLanguage());
+        
+    
+    }
+    
+}
